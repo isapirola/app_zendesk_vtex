66d05c652e9d2095273b6e8851d5182c8ca12d1d
IsaPirola <belapirola@gmail.com>
2024-04-24 13:38:12 -0300

    Create get client data api

--- assets/controllers/app.controller.js
+++ assets/controllers/app.controller.js
@@ -2,5 +2,17 @@ app.controller("AppController", [
     "$scope",
     "zendeskService",
 
-    function ($scope, zendeskService) {},
+    function ($scope, zendeskService) {
+        $scope.pesquisarCliente = function () {
+            zendeskService
+                .getClientData($scope.cpf)
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    // Manipular erros aqui
+                    console.error(error);
+                });
+        };
+    },
 ]);
--- assets/service/zendesk.service.js
+++ assets/service/zendesk.service.js
@@ -1,1 +1,25 @@
-app.service("zendeskService", ["$q", function ($q) {}]);
+app.service("zendeskService", [
+    "$q",
+    function ($q) {
+        this.getClientData = function (cpf) {
+            var deferred = $q.defer();
+            client
+                .request({
+                    url: `https://nOe6xtbvqa.execute-api.us-east-1.amazonaws.com/default/flordemaio-zendesk-mock?cpf=${cpf}`,
+                    type: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "x-api-key": "JBjAVMqtRovzYJ8iRJm559U5mLn9Hw17X5iVCvm9",
+                    },
+                })
+                .then(function (response) {
+                    deferred.resolve(response);
+                })
+                .catch(function (error) {
+                    deferred.reject(error);
+                });
+
+            return deferred.promise;
+        };
+    },
+]);
