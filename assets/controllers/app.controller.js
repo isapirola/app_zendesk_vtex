3c8d6242f83f72ff597039624b1e489ae9a51fdb
IsaPirola <belapirola@gmail.com>
2024-04-25 11:54:30 -0300

    Add CPF validation

--- assets/controllers/app.controller.js
+++ assets/controllers/app.controller.js
@@ -5,6 +5,30 @@ app.controller("AppController", [
     function ($scope, zendeskService) {
         var idsPedidos;
 
+        $scope.validarCPF = function (cpf) {
+            if (!cpf || cpf.length !== 11 || /^(.)\1+$/.test(cpf)) return false;
+
+            // Calcula o primeiro dígito verificador
+            let sum = 0;
+            for (let i = 0; i < 9; i++) {
+                sum += parseInt(cpf.charAt(i)) * (10 - i);
+            }
+            let remainder = 11 - (sum % 11);
+            let digit = remainder > 9 ? 0 : remainder;
+
+            if (parseInt(cpf.charAt(9)) !== digit) return false;
+
+            // Calcula o segundo dígito verificador
+            sum = 0;
+            for (let i = 0; i < 10; i++) {
+                sum += parseInt(cpf.charAt(i)) * (11 - i);
+            }
+            remainder = 11 - (sum % 11);
+            digit = remainder > 9 ? 0 : remainder;
+
+            return parseInt(cpf.charAt(10)) === digit;
+        };
+
         $scope.pesquisarCliente = function () {
             zendeskService
                 .getClientData($scope.cpf)
